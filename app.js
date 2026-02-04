4bb729b49283138b2a95b5c03c5a0217afde48ba
farahmahfouz <farahmahfouz11@gmail.com>
2026-02-04 02:10:07 +0200

    fix: update CORS origin to include default URL for better compatibility

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL ,
+  origin: process.env.CLIENT_URL || 'https://booking-app-inky-eight.vercel.app/',
   credentials: true,
 }));
 
